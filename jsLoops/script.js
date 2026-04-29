56db917306080e037ee10e20c95beced68f62e88
mohiniahirwar2005 <mohiniahirwar828@gmail.com>
2026-04-29 19:25:29 +0530

    loop

---
+++ jsLoops/script.js
@@ -0,0 +1,30 @@
+//let a = [24, 58, 33, 86, 96, 12, 53, 23, 32, 100]
+//for(let i = 0; i<10; i++)
+//{
+  //  console.log(a[i]);
+//}
+
+//let i = 0;
+//while(i<10)
+//{
+  //  console.log(a[i]);
+    //i++;
+//}
+
+
+//for(var i = 0; i<=10; i++)
+//{
+  //  console.log(i*2)
+//}
+//i = 0;
+//while(i<=10)
+//{
+    //console.log(i*2);
+  //  i++;
+//}
+
+i=0;
+do{
+    console.log(i*2);
+    i++;
+}while(i<=10)
